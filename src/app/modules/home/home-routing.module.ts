7009a765eacdbe44f268663b78d042476f93c4df
Subhrodip Mohanta <subhrodip.mohanta@irdeto.com>
2023-06-26 13:15:44 +0530

    Home Module Routing is now working

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{
+const routes: Routes = [ {
   path: '',
-  component: HomeComponent
-}, {
-  path: 'home/scan',
-  outlet: 'home',
-  loadChildren: () => import('./scan/scan.module').then(m => m.ScanModule)
-}, {
-  path: 'user',
-  loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-}, {
-  path: 'admin-meal',
-  loadChildren: () => import('./admin-meal/admin-meal.module').then(m => m.AdminMealModule)
-}, {
-  path: 'admin-users',
-  loadChildren: () => import('./admin-users/admin-users.module').then(m => m.AdminUsersModule)
+  component: HomeComponent,
+  children: [
+    {
+      path: 'scan',
+      loadChildren: () => import('./scan/scan.module').then (m => m.ScanModule)
+    }, {
+      path: 'user',
+      loadChildren: () => import('./user/user.module').then (m => m.UserModule)
+    }, {
+      path: 'admin-meal',
+      loadChildren: () => import('./admin-meal/admin-meal.module').then (m => m.AdminMealModule)
+    }, {
+      path: 'admin-users',
+      loadChildren: () => import('./admin-users/admin-users.module').then (m => m.AdminUsersModule)
+    }
+  ]
 }];
 
 @NgModule({
